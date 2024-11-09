526bb331df764f15c3a2bfc079a75c8b5fcdbcec
Vedant62 <vedant.sharma0510@gmail.com>
2024-11-10 05:10:51 +0530

    opt 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
@@ -8,60 +8,54 @@ const io = new Server(server);
 
 const activeAmbulance = new Map();
 
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+  console.log("socket: ", socket);
 
-io.on('connection', (socket) => {
-    console.log('Client connected:', socket.id);
-    console.log("socket: ",socket);
-
-
-    socket.on('registerAmbulance', (ambulanceData) => {
-        console.log('Succesfully registered!!');
-        activeAmbulance.set(socket.id, {
-            availability:'engaged',
-            ...ambulanceData
-        });
-        io.emit('ambulanceListUpdate', activeAmbulance);
+  socket.on("registerAmbulance", (ambulanceData) => {
+    console.log("Succesfully registered!!");
+    activeAmbulance.set(socket.id, {
+      availability: "engaged",
+      ...ambulanceData,
     });
+    io.emit("ambulanceListUpdate", activeAmbulance);
+  });
 
-
-    socket.on('createEmergencyCase', (caseData) => {
-        // const caseId = generateCaseId(); 
-        // socket.join(`case_${caseId}`);
-        io.to('hospital_staff').emit('newEmergencyCase', {
-            ...caseData
-        });
+  socket.on("createEmergencyCase", (caseData) => {
+    // const caseId = generateCaseId();
+    // socket.join(`case_${caseId}`);
+    io.to("hospital_staff").emit("newEmergencyCase", {
+      ...caseData,
     });
+  });
 
-
-    socket.on('updateVitals', (data) => {
-        io.to(`hospital_staff`).emit('vitalsUpdate', {
-            // timestamp: new Date(),
-            ...data
-        });
+  socket.on("updateVitals", (data) => {
+    io.to(`hospital_staff`).emit("vitalsUpdate", {
+      // timestamp: new Date(),
+      ...data,
     });
+  });
 
+  socket.on("updateLocation", (locationData) => {
+    if (activeAmbulance.has(socket.id)) {
+      activeAmbulance.location = locationData;
+      io.to(`hospital_staff`).emit("ambulanceLocationUpdate", {
+        ...locationData,
+      });
+    }
+  });
 
-
-    socket.on('updateLocation', (locationData) => {
-        if (activeAmbulance.has(socket.id)) {
-            activeAmbulance.location = locationData
-            io.to(`hospital_staff`).emit('ambulanceLocationUpdate', {
-                ...locationData
-            });
-        }
-    });
-
-
-    socket.on('disconnect', () => {
-        if (activeAmbulance.has(socket.id)) {
-            activeAmbulance.delete(socket.id);
-            io.emit('ambulanceListUpdate', Array.from(activeAmbulance.values()));
-        }
-    });
+  socket.on("disconnect", () => {
+    if (activeAmbulance.has(socket.id)) {
+      activeAmbulance.delete(socket.id);
+      io.emit("ambulanceListUpdate", activeAmbulance);
+    }
+    console.log("now active ambulance: ", activeAmbulance);
+    console.log("socket disconnected");
+  });
 });
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
